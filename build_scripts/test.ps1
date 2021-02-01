d96d514a146746e4079e62acac2e6efa11d41d4a
Ketil Wik <ketil.wik@gmail.com>
2021-02-01 17:00:36 +0100

    Update test.ps1

--- build_scripts/test.ps1
+++ build_scripts/test.ps1
@@ -2,9 +2,9 @@ param($clientId)
 
 Write-Host Writing debug values
 
-if($clientId eq 'build' ) {
+if($clientId -eq 'build' ) {
   Write-Host ClientId is build
-} elseif($clientId eq 'test') {
+} elseif($clientId -eq 'test') {
   Write-Host ClientId is test
 } else {
   Write-Host ClientId is unknown
